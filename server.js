87dc88c78e652fddeea23a27d93a382fb0ed4598
YasminAnk <71785328+YasminAnk@users.noreply.github.com>
2024-06-21 23:01:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 10000;
 
+app.use(express.static(__dirname));
+
 app.get('/', function(req, res){
-    res.sendFile('index.html', {
-     root: './'
-   })
+    res.sendFile(__dirname +'index.html');
 });
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
