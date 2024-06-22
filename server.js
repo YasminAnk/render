cba362e0bcde8486bdd7079dff2ac237a855b2f7
YasminAnk <71785328+YasminAnk@users.noreply.github.com>
2024-06-22 17:12:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 const port = process.env.PORT || 10000;
 
 const router = jsonServer.router(__dirname +'db.json');
-
-app.use('/api', router);
+app.use(middlewares);
+app.use('', router);
 
 app.use(express.static(__dirname));
 app.get('/', function(req, res){
