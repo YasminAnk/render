c999b119af233ad99055cba1aa49eea7e9cec43c
YasminAnk <71785328+YasminAnk@users.noreply.github.com>
2024-06-24 12:20:56 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const jsonServer = require("json-server");
+const cors = require("cors");
+const middlewares = jsonServer.defaults();
+
+const app = express();
+const port = process.env.PORT || 10000;
+
+const router = jsonServer.router('./db/db.json');
+app.use(middlewares);
+app.use(router);
+
+app.use(express.static(__dirname));
+app.get('/', function(req, res){
+    res.sendFile(__dirname +'index.html');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
